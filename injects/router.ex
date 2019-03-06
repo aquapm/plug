cc1c04c194299e609c832d01fd255f53e0ce3d6d
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-03-06 21:33:11 +0200

    Adding injects

---
+++ injects/router.ex
@@ -0,0 +1,15 @@
+defmodule <%= @module_alias %> do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/ping" do
+    send_resp(conn, 200, "pong")
+  end
+
+  # Default fallback for unrecognized routes
+  match _ do
+    send_resp(conn, 404, "Not found")
+  end
+end
